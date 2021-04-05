00f0c99620ba0ea914d321c93eac302c978b4fee
AliHajiloo <alihajiloo.ir@gmail.com>
2021-04-05 14:41:17 +0430

    refactor(system-setting): [app-table]

--- src/app/services/feature-services/system-setting-services/organization-supervisor.service.ts
+++ src/app/services/feature-services/system-setting-services/organization-supervisor.service.ts
@@ -9,21 +9,21 @@ import { ApiClientService } from '../../Base/api-client.service';
 export class OrganizationSupervisorService extends Specification {
     private static organizationSupervisorApi = '/api/v1/organization-supervisor';
 
-    getOrganization(fc?: FormContainer) {
+    get(fc?: FormContainer) {
         const api = OrganizationSupervisorService.organizationSupervisorApi + this.generateSpecificationString();
         return this.apiClientService.get(api, fc);
     }
 
-    deleteOrganization(id, fc?: FormContainer) {
+    delete(id, fc?: FormContainer) {
         const api = OrganizationSupervisorService.organizationSupervisorApi + '/' + id;
         return this.apiClientService.delete(api, fc);
     }
 
-    updateOrganization(model, fc?: FormContainer) {
+    update(model, fc?: FormContainer) {
         return this.apiClientService.put(OrganizationSupervisorService.organizationSupervisorApi, fc, model);
     }
 
-    createOrganization(model, fc?: FormContainer) {
+    create(model, fc?: FormContainer) {
         return this.apiClientService.post(OrganizationSupervisorService.organizationSupervisorApi, model, fc);
     }
 
