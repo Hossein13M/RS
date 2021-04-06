59b9e56db2b01b49b1c5371c7c05619c47164978
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-04-06 13:30:20 +0430

    fix(portfolio-management)!: fixed an issue where trade-book-history.component.ts was importing from out of date model file

--- src/app/modules/portfolio-management/trade-book/trade-book-history/trade-book-history.component.ts
+++ src/app/modules/portfolio-management/trade-book/trade-book-history/trade-book-history.component.ts
@@ -2,8 +2,8 @@ import { formatDate } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { PaginationChangeType } from '../../../../shared/components/table/table-consts';
-import { StateManager } from '../../../../shared/pipes/stateManager.pipe';
+import { PaginationChangeType } from '#shared/components/table/table.model';
+import { StateManager } from '#shared/pipes/stateManager.pipe';
 import { TradeBookHistoryService } from './trade-book-history.service';
 
 @Component({
