b30a86805cc09c9a291612f2829e7a576dc3de51
hossein13m <hossein13mousavi@gmail.com>
2021-04-10 14:43:46 +0430

    refactor: refactor op risk reporting

--- src/app/modules/op-risk/op-risk-reporting/op-risk-reporting.service.ts
+++ src/app/modules/op-risk/op-risk-reporting/op-risk-reporting.service.ts
@@ -1,31 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class OpRiskReportingService {
-    private latestMappingSubject = new BehaviorSubject<any>(null);
-
     constructor(private http: HttpClient) {}
 
-    getTops(isRisk: boolean) {
+    public getTops(isRisk: boolean) {
         return this.http.get<Array<{ items: Array<{ titleFA: string; treeNodeId: number; maxCount: string }>; name: string }>>(
             `/api/v1/opRiskReporting/Tops?isRisk=${isRisk}`
         );
     }
 
-    getPieChart(id: number, isRisk: boolean, table: string) {
+    public getPieChart(id: number, isRisk: boolean, table: string) {
         return this.http.get<any>(`/api/v1/opRiskReporting/pieChart?id=${id}&isRisk=${isRisk}&table=${table}`);
     }
 
-    getAxis(route: string) {
+    public getAxis(route: string) {
         return this.http.get<Array<{ titleFA: string; type: string }>>(`/api/v1/opRiskReporting/${route}`);
     }
 
-    getBarChart(xAxis: string, yAxis: string, isRisk: boolean) {
-        return this.http
-            .get<any>(`/api/v1/opRiskReporting/barChart?xAxis=${xAxis}&yAxis=${yAxis}&isRisk=${isRisk}`)
-            .pipe(tap((mapping) => this.latestMappingSubject.next(mapping)));
+    public getBarChart(xAxis: string, yAxis: string, isRisk: boolean) {
+        return this.http.get<any>(`/api/v1/opRiskReporting/barChart?xAxis=${xAxis}&yAxis=${yAxis}&isRisk=${isRisk}`);
     }
 }
