76946489bfcb72816ed546d0ab36d6245e4a5182
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-04-18 10:32:11 +0430

    feat: [incomplete] search with both Tamadon and Bazargardani in risk measuring page

--- src/app/modules/nav/risk-measuring.service.ts
+++ src/app/modules/nav/risk-measuring.service.ts
@@ -4,6 +4,13 @@ import { FormContainer } from 'app/shared/models/FromContainer';
 import { PageEvent, SpecificationModel } from 'app/shared/models/Specification';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+interface RiskManagementValue {
+    owner: string | Array<string>;
+    data: string;
+    confidenceInterval: string | number;
+}
 
 @Injectable({
     providedIn: 'root',
@@ -28,11 +35,28 @@ export class RiskMeasuringService {
         return this.latestMappingSubject.getValue();
     }
 
-    constructor(private acs: ApiClientService) {}
+    constructor(private acs: ApiClientService, private http: HttpClient) {}
 
     getYieldCurve(owner, date, confidence, fc?: FormContainer): Observable<any> {
         return this.acs
             .get(RiskMeasuringService.valueAtRiskApi + `?owner=${owner}&date=${date}&confidenceInterval=${confidence}`, fc)
             .pipe(tap((mapping) => this.latestMappingSubject.next(mapping)));
+    }
+
+    public getRiskManagementValues(searchParams: RiskManagementValue): Observable<any> {
+        const params: HttpParams = this.prepareParamsForObjects(searchParams);
+        return this.http.get(`/api/value-at-risk/`, { params });
+    }
+
+    private prepareParamsForObjects(searchParams: any): HttpParams {
+        let params: HttpParams = new HttpParams();
+        Object.keys(searchParams).map((key: string) => {
+            if (Array.isArray(searchParams[key])) {
+                searchParams[key].forEach((element) => params.append(key, element));
+            } else if (searchParams[key] !== '') {
+                params = params.append(key, searchParams[key]);
+            }
+        });
+        return params;
     }
 }
--- src/app/modules/nav/risk-measuring/risk-measuring.component.ts
+++ src/app/modules/nav/risk-measuring/risk-measuring.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { RiskMeasuringService } from '../risk-measuring.service';
 
 @Component({
@@ -10,133 +10,70 @@ import { RiskMeasuringService } from '../risk-measuring.service';
 export class RiskMeasuringComponent implements OnInit {
     show = false;
     loading = true;
-    formGroup: FormGroup;
-
+    form: FormGroup = this.fb.group({
+        date: new FormControl(new Date('2019-01-30')),
+        owner: new FormControl('', [Validators.required]),
+        confidenceInterval: new FormControl('99'),
+    });
     barChart: any;
     gage: any;
 
     public chartType: string = 'line';
     public chartDatasets: Array<any> = [{ data: [], label: '' }];
     public chartLabels: Array<any> = [];
-    public chartColors: Array<any> = [
-        {
-            backgroundColor: 'transparent',
-            borderColor: this.randomColor(),
-            borderWidth: 1,
-        },
-    ];
-    public chartOptions: any = {
-        responsive: true,
-    };
+    public chartColors: Array<any> = [{ backgroundColor: 'transparent', borderColor: this.randomColor(), borderWidth: 1 }];
+    public chartOptions: any = { responsive: true };
 
-    showingDataColumns = [
-        {
-            // NO Search Option Example
-            name: 'VaR',
-            id: 'VaR',
-            type: 'string',
-            minWidth: '130px',
-            headerAlign: 'center',
-            dataAlign: 'center',
-        },
-        {
-            name: 'Upper bound VaR',
-            id: 'upperBoundVar',
-            type: 'string',
-            minWidth: '150px',
-            headerAlign: 'center',
-            dataAlign: 'center',
-        },
-        {
-            name: 'worstVaR',
-            id: 'worstVaR',
-            type: 'string',
-            minWidth: '150px',
-            headerAlign: 'center',
-            dataAlign: 'center',
-        },
-        {
-            name: 'Provision',
-            id: 'provision',
-            type: 'string',
-            minWidth: '150px',
-            headerAlign: 'center',
-            dataAlign: 'center',
-        },
-    ];
     showingData = [];
 
+    showingDataColumns = [
+        { name: 'VaR', id: 'VaR', type: 'string', minWidth: '130px', headerAlign: 'center', dataAlign: 'center' },
+        { name: 'Upper bound VaR', id: 'upperBoundVar', type: 'string', minWidth: '150px', headerAlign: 'center', dataAlign: 'center' },
+        { name: 'worstVaR', id: 'worstVaR', type: 'string', minWidth: '150px', headerAlign: 'center', dataAlign: 'center' },
+        { name: 'Provision', id: 'provision', type: 'string', minWidth: '150px', headerAlign: 'center', dataAlign: 'center' },
+    ];
+
     showingDataColumns2 = [
-        {
-            // NO Search Option Example
-            name: 'VaR/سرمایه',
-            id: 'VaR',
-            type: 'string',
-            minWidth: '130px',
-            headerAlign: 'center',
-            dataAlign: 'center',
-        },
-        {
-            name: 'Upper bound VaR/سرمایه',
-            id: 'upperBoundVar',
-            type: 'string',
-            minWidth: '150px',
-            headerAlign: 'center',
-            dataAlign: 'center',
-        },
-        {
-            name: 'worst VaR/سرمایه',
-            id: 'worstVaR',
-            type: 'string',
-            minWidth: '150px',
-            headerAlign: 'center',
-            dataAlign: 'center',
-        },
-        {
-            name: 'Provision/سرمایه',
-            id: 'provision',
-            type: 'string',
-            minWidth: '150px',
-            headerAlign: 'center',
-            dataAlign: 'center',
-        },
+        { name: 'VaR/سرمایه', id: 'VaR', type: 'string', minWidth: '130px', headerAlign: 'center', dataAlign: 'center' },
+        { name: 'Upper bound VaR/سرمایه', id: 'upperBoundVar', type: 'string', minWidth: '150px', headerAlign: 'center', dataAlign: 'center' },
+        { name: 'worst VaR/سرمایه', id: 'worstVaR', type: 'string', minWidth: '150px', headerAlign: 'center', dataAlign: 'center' },
+        { name: 'Provision/سرمایه', id: 'provision', type: 'string', minWidth: '150px', headerAlign: 'center', dataAlign: 'center' },
     ];
     showingData2 = [];
 
-    constructor(private readonly riskMeasuringService: RiskMeasuringService) {}
+    constructor(private readonly riskMeasuringService: RiskMeasuringService, private fb: FormBuilder) {}
 
-    ngOnInit(): void {
-        this.formGroup = new FormGroup({
-            date: new FormControl(new Date('2019-01-30')),
-            owner: new FormControl('', [Validators.required]),
-            confidenceInterval: new FormControl('99'),
-        });
-    }
+    ngOnInit(): void {}
 
-    get() {
+    public get() {
+        let searchParams = this.form.value;
         this.loading = false;
-        const date = this.formGroup.value.date.toISOString().split('T')[0];
-        const owner = this.formGroup.value.owner;
-        const confidence = this.formGroup.value.confidenceInterval;
-        this.riskMeasuringService.getYieldCurve(owner, date, confidence).subscribe((response) => {
-            this.loading = true;
-            this.show = true;
-            this.barChart = response.histogramPL.dataset[0].data;
-            this.gage = response.VaR.toFixed(3);
-            this.createChart(response.historicalVaR.plot);
-            this.showingData.push({
-                VaR: response.VaR_rial,
-                upperBoundVar: response.upperBound_VaR_rial,
-                worstVaR: response.worstVaR_rial_ratio,
-                provision: response.provision,
-            });
-            this.showingData2.push({
-                VaR: response.VaR_rial_ratio,
-                upperBoundVar: response.upperBound_VaR_rial,
-                worstVaR: response.worstVaR_rial_ratio,
-                provision: response.provision_ratio,
-            });
-        });
+        searchParams.date = this.form.value.date.toISOString().split('T')[0];
+        console.log(searchParams);
+        // const date = this.form.value.date.toISOString().split('T')[0];
+        const owner = this.form.value.owner;
+        const confidence = this.form.value.confidenceInterval;
+        this.riskMeasuringService.getRiskManagementValues(searchParams).subscribe((result) => console.log('result is: ', result));
+
+        // this.riskMeasuringService.getYieldCurve(owner, date, confidence).subscribe((response) => {
+        //     this.loading = true;
+        //     this.show = true;
+        //     this.barChart = response.histogramPL.dataset[0].data;
+        //     this.gage = response.VaR.toFixed(3);
+        //     this.createChart(response.historicalVaR.plot);
+        //     this.showingData.push({
+        //         VaR: response.VaR_rial,
+        //         upperBoundVar: response.upperBound_VaR_rial,
+        //         worstVaR: response.worstVaR_rial_ratio,
+        //         provision: response.provision,
+        //     });
+        //     this.showingData2.push({
+        //         VaR: response.VaR_rial_ratio,
+        //         upperBoundVar: response.upperBound_VaR_rial,
+        //         worstVaR: response.worstVaR_rial_ratio,
+        //         provision: response.provision_ratio,
+        //     });
+        // });
     }
 
     createChart(dataArray) {
@@ -144,14 +81,8 @@ export class RiskMeasuringComponent implements OnInit {
         const data = [];
         for (let i = 0; i < dataArray.length; i++) {
             data.push(dataArray[i].VaR);
-            const date = new Date(dataArray[i].date).toLocaleDateString('fa-Ir', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-            });
-            if (!labels.includes(date)) {
-                labels.push(date);
-            }
+            const date = new Date(dataArray[i].date).toLocaleDateString('fa-Ir', { year: 'numeric', month: 'long', day: 'numeric' });
+            if (!labels.includes(date)) labels.push(date);
         }
         this.chartDatasets[0].data = data;
         this.chartLabels = labels;
@@ -161,7 +92,6 @@ export class RiskMeasuringComponent implements OnInit {
         const x = Math.floor(Math.random() * 256);
         const y = Math.floor(Math.random() * 256);
         const z = Math.floor(Math.random() * 256);
-        const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
-        return bgColor;
+        return 'rgb(' + x + ',' + y + ',' + z + ')';
     }
 }
