5f021722991e604e3636b9cad67c4430a264ba2b
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-04-19 18:59:33 +0430

    refactor: refactor risk measurement

---
+++ src/app/modules/risk-measurement/yield-curve.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class YieldCurveService {
+    constructor(private http: HttpClient) {}
+
+    public getYieldCurveData(date: string): Observable<any> {
+        return this.http.get(`/api/v1/yield-curve`, { params: { date: date } });
+    }
+}
