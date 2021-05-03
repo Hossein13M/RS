9a9cf3af630b7321d21a4c4ec828949457e587bf
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-05-03 13:37:39 +0430

    fix: remove update date from assets monitoring

--- src/app/shared/pipes/price.pipe.ts
+++ src/app/shared/pipes/price.pipe.ts
@@ -9,9 +9,8 @@ export class PricePipe implements PipeTransform {
     constructor(private pricePipeService: PricePipeService) {}
 
     public static convertPrice(value: any): string {
-        if (!value) {
-            return '-';
-        }
+        if (!value) return '-';
+
         return (
             '﷼' +
             value
@@ -22,9 +21,7 @@ export class PricePipe implements PipeTransform {
     }
 
     transform(price: any): string {
-        if (!price) {
-            return '-';
-        }
+        if (!price) return '-';
 
         let priceNumber;
         this.pricePipeService.downScaleOrder === 0 ? (priceNumber = parseFloat(price.toFixed())) : (priceNumber = parseFloat(price));
