e66397715649dfc09c38f1724ba7a9e843614f97
naixsenapi <naixsenpai@gmail.com>
2021-05-18 14:33:29 +0430

    refactor(system-setting): refactor of gl-setting.service.ts

--- src/app/modules/features/system-setting/gl-setting-components/gl-setting.service.ts
+++ src/app/modules/features/system-setting/gl-setting-components/gl-setting.service.ts
@@ -5,26 +5,22 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlSettingService {
-    private static glSettingApi = '/api/v1/inst-gl-mapping';
-
     constructor(private http: HttpClient) {}
 
     public createGlSetting(model): Observable<any> {
-        return this.http.post(GlSettingService.glSettingApi, model);
+        return this.http.post('/api/v1/inst-gl-mapping', model);
     }
 
     public deleteGlSetting(id): Observable<any> {
-        const api = GlSettingService.glSettingApi + '/' + id;
-        return this.http.delete(api);
+        return this.http.delete('/api/v1/inst-gl-mapping/' + id);
     }
 
     public updateGlSetting(model): Observable<any> {
-        return this.http.put(GlSettingService.glSettingApi, model);
+        return this.http.put('/api/v1/inst-gl-mapping', model);
     }
 
     public getGlSettings(paginationParams, searchParams?): Observable<any> {
         const params: HttpParams = UtilityFunctions.prepareParamsFromObjectsForAPICalls({ ...paginationParams, ...searchParams });
-        const api = GlSettingService.glSettingApi;
-        return this.http.get(api, { params });
+        return this.http.get('/api/v1/inst-gl-mapping', { params });
     }
 }
