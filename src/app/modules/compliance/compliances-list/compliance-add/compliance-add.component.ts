b4b3d37d45f68591ee0239fb25e95bcc5643dedc
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-05-19 22:43:28 +0430

    refactor(compliances): changed the location of the compliance-add.component

--- src/app/modules/compliance/compliances-list/compliance-add/compliance-add.component.ts
+++ src/app/modules/compliance/compliances-list/compliance-add/compliance-add.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { CompliancesService } from '../compliances.service';
-import { AlertService } from '../../../services/alert.service';
+import { CompliancesService } from '../../compliances.service';
+import { AlertService } from '../../../../services/alert.service';
 
 @Component({
     templateUrl: './compliance-add.component.html',
