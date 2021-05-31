f50f1c5b3bec4f42338cb50c1799d8d9161cefd0
hossein13m <hossein13mousavi@gmail.com>
2021-05-31 15:27:13 +0430

    feat(organization-structure): remote useless modules and imports

--- src/app/modules/organizations-structure/organizations-list/organizations-list.module.ts
+++ src/app/modules/organizations-structure/organizations-list/organizations-list.module.ts
@@ -3,11 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { OrganizationsListComponent } from './organizations-list.component';
 import { LayoutModule } from '../../../layout/layout.module';
-import { MatDividerModule } from '@angular/material/divider';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexModule } from '@angular/flex-layout';
 import { OrganizationModule } from '../organization/organization.module';
@@ -21,11 +17,7 @@ const routes: Routes = [{ path: '', pathMatch: 'full', component: OrganizationsL
         CommonModule,
         RouterModule.forChild(routes),
         LayoutModule,
-        MatDividerModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
         MatIconModule,
-        MatInputModule,
         MatButtonModule,
         FlexModule,
         OrganizationModule,
--- src/app/modules/organizations-structure/roles-list/roles-list.component.ts
+++ src/app/modules/organizations-structure/roles-list/roles-list.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-role',
-  templateUrl: './roles-list.component.html',
-  styleUrls: ['./roles-list.component.scss']
+    selector: 'app-role',
+    templateUrl: './roles-list.component.html',
+    styleUrls: ['./roles-list.component.scss'],
 })
 export class RolesListComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
