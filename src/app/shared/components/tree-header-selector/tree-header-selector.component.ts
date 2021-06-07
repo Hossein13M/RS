dbcc55f2d205c7ebff8a4f1c662e1b99eea6730b
hossein13m <hossein13mousavi@gmail.com>
2021-06-07 10:07:40 +0430

    merge: resolve merge conflicts from feat/organization to dev

---
+++ src/app/shared/components/tree-header-selector/tree-header-selector.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-tree-header-selector',
+    templateUrl: './tree-header-selector.component.html',
+    styleUrls: ['./tree-header-selector.component.scss'],
+})
+export class TreeHeaderSelectorComponent implements OnInit {
+    @Input() selected: string;
+    @Input() organizationId: string | number;
+    @Input() headerList: Array<{ title: string; icon: string; englishTitle: string }>;
+
+    @Output() changedSection = new EventEmitter();
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    public changeSection(headerTitle: string): void {
+        this.changedSection.emit({ selectedSection: headerTitle });
+    }
+}
