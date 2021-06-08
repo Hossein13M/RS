14c2c8c10ff806019af1ac9f7a0b7c2505015131
hossein13m <hossein13mousavi@gmail.com>
2021-06-08 15:53:14 +0430

    fix(organization-structure): implement mapping

--- src/app/modules/organizations-structure/organization-structure.module.ts
+++ src/app/modules/organizations-structure/organization-structure.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrganizationStructureRoutingModule } from './organization-structure.routing.module';
 import { OrganizationStructureService } from './organization-structure.service';
+import { OrganizationRoleMappingDialogModule } from './organization-role-mapping-dialog/organization-role-mapping-dialog.module';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, OrganizationStructureRoutingModule],
+    imports: [CommonModule, OrganizationStructureRoutingModule, OrganizationRoleMappingDialogModule],
     providers: [OrganizationStructureService],
 })
 export class OrganizationStructureModule {}
--- src/app/modules/organizations-structure/roles-list/roles-list.module.ts
+++ src/app/modules/organizations-structure/roles-list/roles-list.module.ts
@@ -5,7 +5,6 @@ import { RolesListComponent } from './roles-list.component';
 import { LayoutModule } from '../../../layout/layout.module';
 import { FlexModule } from '@angular/flex-layout';
 import { ShareModule } from '#shared/share.module';
-import { TreeChartModule } from '../../op-risk/tree-chart/tree-chart.module';
 import { TreeHeaderSelectorModule } from '#shared/components/tree-header-selector/tree-header-selector.module';
 import { TreeSimpleSelectorModule } from '#shared/components/tree-simple-selector/tree-simple-selector.module';
 
