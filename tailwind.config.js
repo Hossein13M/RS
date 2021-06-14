4a7599f55e746f72901746d2946690c898138f93
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-06-14 15:53:35 +0430

    style(tailwind): added playground and completed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,32 @@
 const { guessProductionMode } = require('@ngneat/tailwind');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
     prefix: 'tw-',
+    important: true,
     purge: {
         enabled: guessProductionMode(),
         content: ['./src/**/*.{html,ts}'],
     },
     darkMode: 'class', // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                blue: '0 1px 3px 0 ' + colors.blue['100'],
+                'blue-lg': '0 10px 15px -3px ' + colors.blue['200'],
+                green: '0 1px 3px 0 ' + colors.green['100'],
+                'green-lg': '0 10px 15px -3px ' + colors.green['200'],
+                yellow: '0 1px 3px 0 ' + colors.yellow['100'],
+                'yellow-lg': '0 10px 15px -3px ' + colors.yellow['200'],
+                pink: '0 1px 3px 0 ' + colors.pink['100'],
+                'pink-lg': '0 10px 15px -3px ' + colors.pink['200'],
+            },
+        },
     },
     variants: {
-        extend: {},
+        extend: {
+            backgroundColor: ['even', 'odd'],
+        },
     },
     corePlugins: {
         preflight: false, // Todo(tailwind): unexpected behaviour in buttons
