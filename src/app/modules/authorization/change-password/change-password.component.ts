0d29dac767b39aae06b13000228c6c3f80f896f1
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-07-03 19:24:50 +0430

    feat(authorization): fixed change-password.component

--- src/app/modules/authorization/change-password/change-password.component.ts
+++ src/app/modules/authorization/change-password/change-password.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FuseConfigService } from '../../../../@fuse/services/config.service';
+import { fuseAnimations } from '../../../../@fuse/animations';
 // @ts-ignore
 import version from '../../../../../package.json';
-import {fuseAnimations} from "../../../../@fuse/animations";
 
 @Component({
-    selector: 'app-change-password',
+    selector: 'change-password',
     templateUrl: './change-password.component.html',
     styleUrls: ['./change-password.component.scss'],
     encapsulation: ViewEncapsulation.None,
