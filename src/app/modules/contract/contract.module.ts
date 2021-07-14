a016b4422870a9f3903a4acfa3bc4852794be25c
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-07-14 15:38:07 +0430

    feat(contract): contract type list implementation

--- src/app/modules/contract/contract.module.ts
+++ src/app/modules/contract/contract.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ContractTypeModule } from './contract-type/contract-type.module';
 import { ContractListModule } from './contract-list/contract-list.module';
 import { ContractRoutingModule } from './contract-routing.module';
+import { ContractService } from './contract.service';
 
 @NgModule({
     declarations: [],
     imports: [CommonModule, ContractRoutingModule, ContractTypeModule, ContractListModule],
+    providers: [ContractService],
 })
 export class ContractModule {}
---
+++ src/app/modules/contract/contract.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UtilityFunctions } from '#shared/utilityFunctions';
+import { ContractType } from './contract-type/contract.model';
+import { ResponseWithPagination } from '#shared/models/pagination.model';
+
+@Injectable()
+export class ContractService {
+    constructor(private http: HttpClient) {}
+
+    public getContractTypes(searchParams: { organization: number; id?: string; name?: string }): Observable<ResponseWithPagination<ContractType>> {
+        const params = UtilityFunctions.prepareParamsFromObjectsForAPICalls(searchParams);
+        return this.http.get<ResponseWithPagination<ContractType>>('http://172.22.255.239:3006/api/v1/contract-type', { params });
+    }
+
+    public changeContractTypeStatus(id: string): Observable<any> {
+        return this.http.put<any>(`http://172.22.255.239:3006/api/v1/contract-type/inactive/${id}`, {});
+    }
+}
