2433005e8aff753d7747f1477c6571f2f7c58f52
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-07-27 17:00:00 +0430

    feat(bpmn): bpmn base

--- src/app/modules/contract/bpmn/bpmn.component.ts
+++ src/app/modules/contract/bpmn/bpmn.component.ts
@@ -16,6 +16,10 @@ export class BpmnComponent implements OnInit {
     public flowDetails: Flow;
     private organizationCode: number = UtilityFunctions.getActiveOrganizationInfo('code');
 
+    //
+    importError?: Error;
+    diagramUrl = 'https://cdn.staticaly.com/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
+
     constructor(private activatedRoute: ActivatedRoute, private flowService: FlowService) {}
 
     ngOnInit(): void {
@@ -36,5 +40,20 @@ export class BpmnComponent implements OnInit {
 
     public saveBPMN(): void {
         console.log('kir');
+    }
+
+    public handleImported(event) {
+        console.log(event);
+        const { type, error, warnings } = event;
+
+        if (type === 'success') {
+            console.log(`Rendered diagram (%s warnings)`, warnings.length);
+        }
+
+        if (type === 'error') {
+            console.error('Failed to render diagram', error);
+        }
+
+        this.importError = error;
     }
 }
--- src/app/modules/contract/bpmn/bpmn.module.ts
+++ src/app/modules/contract/bpmn/bpmn.module.ts
@@ -6,11 +6,12 @@ import { LayoutModule } from '../../../layout/layout.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BpmnDiagramComponent } from './bpmn-diagram/bpmn-diagram.component';
 
 const routes: Routes = [{ path: '', pathMatch: 'full', component: BpmnComponent }];
 
 @NgModule({
-    declarations: [BpmnComponent],
+    declarations: [BpmnComponent, BpmnDiagramComponent],
     imports: [CommonModule, RouterModule.forChild(routes), LayoutModule, MatIconModule, MatButtonModule, MatProgressSpinnerModule],
     exports: [BpmnComponent],
 })
