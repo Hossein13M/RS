70b99bf267ad2fb38ecdb4768e2d5a49a3b56c78
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-07-27 12:17:27 +0430

    feat(bpmn): initialize BPMN component

--- src/app/modules/contract/bpmn/bpmn.module.ts
+++ src/app/modules/contract/bpmn/bpmn.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BpmnComponent } from './bpmn.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutModule } from '../../../layout/layout.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const routes: Routes = [{ path: '', pathMatch: 'full', component: BpmnComponent }];
 
 @NgModule({
     declarations: [BpmnComponent],
-    imports: [CommonModule, RouterModule.forChild(routes)],
+    imports: [CommonModule, RouterModule.forChild(routes), LayoutModule, MatIconModule, MatButtonModule, MatProgressSpinnerModule],
     exports: [BpmnComponent],
 })
 export class BpmnModule {}
