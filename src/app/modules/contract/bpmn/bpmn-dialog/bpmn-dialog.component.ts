59127d999e8578e235a50f5b7b8a83efd766d1a0
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-08-02 12:11:06 +0430

    chore(bpmnDialog): init bpmn dialog component

---
+++ src/app/modules/contract/bpmn/bpmn-dialog/bpmn-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-bpmn-dialog',
+    templateUrl: './bpmn-dialog.component.html',
+    styleUrls: ['./bpmn-dialog.component.scss'],
+})
+export class BpmnDialogComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
