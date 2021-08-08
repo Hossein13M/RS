7bd5ae2907061e3e4d593ec3f2e6109b8d0d4966
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-08-08 11:55:39 +0430

    fix(contractType): BPMN diagram changed and implemented

--- src/app/modules/contract/flow/flow.model.ts
+++ src/app/modules/contract/flow/flow.model.ts
@@ -2,7 +2,8 @@ export interface Flow {
     name: string;
     isManual: boolean;
     organization: number;
-    _id: string;
+    id?: string;
+    _id?: string;
     isActive: boolean;
     createdAt: string;
     updatedAt: string;
