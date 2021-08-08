3b7e48abb8e358a69aa7d6b4d1e876a07061e791
hossein13m <hossein13mousavi@gmail.com>
2021-08-08 11:58:08 +0430

    chore(BPMN): remove BPMN Diagram Component

--- src/app/modules/contract/bpmn/bpmn.module.ts
+++ src/app/modules/contract/bpmn/bpmn.module.ts
@@ -6,7 +6,6 @@ import { LayoutModule } from '../../../layout/layout.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { BpmnDiagramComponent } from './bpmn-diagram/bpmn-diagram.component';
 import { BpmnDialogComponent } from './bpmn-dialog/bpmn-dialog.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -21,8 +20,24 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 const routes: Routes = [{ path: '', pathMatch: 'full', component: BpmnComponent }];
 
 @NgModule({
-    declarations: [BpmnComponent, BpmnDiagramComponent, BpmnDialogComponent],
-    imports: [CommonModule, RouterModule.forChild(routes), LayoutModule, MatIconModule, MatButtonModule, MatProgressSpinnerModule, MatToolbarModule, MatDialogModule, FlexModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatSelectModule, ShareModule, DragDropModule],
+    declarations: [BpmnComponent, BpmnDialogComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        LayoutModule,
+        MatIconModule,
+        MatButtonModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        MatDialogModule,
+        FlexModule,
+        MatCardModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        ShareModule,
+        DragDropModule,
+    ],
     exports: [BpmnComponent],
 })
 export class BpmnModule {}
