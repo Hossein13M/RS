9c232367576ebd11398c4ff647bb132f6d1bafa1
hossein13m <hossein13mousavi@gmail.com>
2021-09-08 14:56:17 +0430

    feat(layout): change theming

--- src/app/layout/vertical/layout-1/layout-1.component.ts
+++ src/app/layout/vertical/layout-1/layout-1.component.ts
@@ -13,55 +13,19 @@ import { navigation } from 'app/dashboard-configs/navigation';
 export class VerticalLayout1Component implements OnInit, OnDestroy {
     fuseConfig: any;
     navigation: any;
-
-    // Private
     private _unsubscribeAll: Subject<any>;
 
-    /**
-     * Constructor
-     *
-     * @param {FuseConfigService} _fuseConfigService
-     */
     constructor(private _fuseConfigService: FuseConfigService) {
-        // Set the defaults
-
-        const userRoles = JSON.parse(localStorage.getItem('user'))  ?? { role: 'somethingElse' };
-
-        if (userRoles.role === 'assets') {
-            this.navigation = [navigation[2]];
-        } else {
-            this.navigation = navigation;
-        }
-
-        // navigation.forEach((navItem) => {
-        //     userRoles.split('|').map((item) => {
-        //         if (item === navItem.id) {
-        //             this.navigation.push(navItem);
-        //         }
-        //     });
-        // });
-
-        // Set the private defaults
+        const userRoles = JSON.parse(localStorage.getItem('user')) ?? { role: 'somethingElse' };
+        userRoles.role === 'assets' ? (this.navigation = [navigation[2]]) : (this.navigation = navigation);
         this._unsubscribeAll = new Subject();
     }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * On init
-     */
     ngOnInit(): void {
-        // Subscribe to config changes
         this._fuseConfigService.config.pipe(takeUntil(this._unsubscribeAll)).subscribe((config) => (this.fuseConfig = config));
     }
 
-    /**
-     * On destroy
-     */
     ngOnDestroy(): void {
-        // Unsubscribe from all subscriptions
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
     }
