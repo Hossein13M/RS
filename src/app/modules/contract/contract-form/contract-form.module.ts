fa1a8720933dfda6c9b087eb29ba264f5d338591
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-09-13 09:28:16 +0430

    release new version of ag-form-builder now we have datepicker

--- src/app/modules/contract/contract-form/contract-form.module.ts
+++ src/app/modules/contract/contract-form/contract-form.module.ts
@@ -3,18 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { AgFormBuilderModule } from 'ag-form-builder';
 import { ContractFormComponent } from './contract-form.component';
 import { ContractFormDialogComponent } from './contract-form-dialog/contract-form-dialog.component';
 import { ContractFormService } from './contract-form.service';
 import { LayoutModule } from '../../../layout/layout.module';
 import { ShareModule } from '#shared/share.module';
+import { AgFormBuilderModule } from 'ag-form-builder';
 
 const routes: Routes = [{ path: '', pathMatch: 'full', component: ContractFormComponent }];
 
 @NgModule({
     declarations: [ContractFormComponent, ContractFormDialogComponent],
-    imports: [CommonModule, RouterModule.forChild(routes), LayoutModule, MatButtonModule, MatIconModule, AgFormBuilderModule, ShareModule],
+    imports: [CommonModule, RouterModule.forChild(routes), LayoutModule, MatButtonModule, MatIconModule, ShareModule, AgFormBuilderModule],
     entryComponents: [ContractFormDialogComponent],
     providers: [ContractFormService],
 })
