924cf4b36a01b06d8b274800820685ea470f0e16
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-09-18 14:42:44 +0430

    feat(cardboard): change action icon colors

--- src/app/modules/contract/contract-cardboard/cardboard/cardboard-history/cardboard-history.actions.ts
+++ src/app/modules/contract/contract-cardboard/cardboard/cardboard-history/cardboard-history.actions.ts
@@ -2,12 +2,12 @@ import { ThemePalette } from '@angular/material/core';
 
 export const ContractHistoryIcons: Array<Action> = [
     { typePer: 'شروع', icon: 'lens', iconColor: 'warn', titleText: 'آغاز قرارداد' },
-    { typePer: 'پایان', icon: 'check_circle_outline', iconColor: 'accent', titleText: 'پایان قرارداد' },
+    { typePer: 'پایان', icon: 'check_circle_outline', iconColor: 'primary', titleText: 'پایان قرارداد' },
     { typePer: 'ایجاد کد', icon: 'code', iconColor: 'primary', titleText: 'ساختن کد قرارداد' },
     { typePer: 'متوقف', icon: 'pause_circle_filled', iconColor: 'warn', titleText: 'متوقف‌کردن قرارداد' },
     { typePer: 'بازگشایی', icon: 'play_circle_filled', iconColor: 'accent', titleText: 'بازگشایی قرارداد' },
     { typePer: 'رد', icon: 'thumb_down_alt', iconColor: 'warn', titleText: 'رد کردن قرارداد' },
-    { typePer: 'تایید', icon: 'thumb_up_off_alt', iconColor: 'accent', titleText: 'تایید قرارداد' },
+    { typePer: 'تایید', icon: 'thumb_up_off_alt', iconColor: 'primary', titleText: 'تایید قرارداد' },
     { typePer: 'آپلود', icon: 'publish', iconColor: 'warn', titleText: 'آپلود قرارداد' },
     { typePer: 'دانلود', icon: 'get_app', iconColor: 'warn', titleText: 'دانلود قرارداد' },
 ];
