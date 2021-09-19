3ec5298d43c114fbe3bcf215b08ee28c8e96f62c
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-09-19 14:12:02 +0430

    refactor(global): enhance codes in theme option modal

--- src/@fuse/components/theme-options/theme-options.module.ts
+++ src/@fuse/components/theme-options/theme-options.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatOptionModule } from '@angular/material/core';
@@ -11,11 +10,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
 import { FuseDirectivesModule } from '@fuse/directives/directives';
 import { FuseMaterialColorPickerModule } from '@fuse/components/material-color-picker/material-color-picker.module';
 import { FuseSidebarModule } from '@fuse/components/sidebar/sidebar.module';
-
 import { FuseThemeOptionsComponent } from '@fuse/components/theme-options/theme-options.component';
 
 @NgModule({
@@ -24,9 +21,6 @@ import { FuseThemeOptionsComponent } from '@fuse/components/theme-options/theme-
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
-
-        FlexLayoutModule,
-
         MatButtonModule,
         MatCheckboxModule,
         MatDividerModule,
@@ -36,7 +30,6 @@ import { FuseThemeOptionsComponent } from '@fuse/components/theme-options/theme-
         MatRadioModule,
         MatSelectModule,
         MatSlideToggleModule,
-
         FuseDirectivesModule,
         FuseMaterialColorPickerModule,
         FuseSidebarModule,
