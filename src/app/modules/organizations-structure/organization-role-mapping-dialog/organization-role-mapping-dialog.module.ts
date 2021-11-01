dfd247252adeaeb80c6e151ecd94da0d727fe5bf
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-11-01 11:08:14 +0330

    refactor(organization): remove fxFlex and enhance header

--- src/app/modules/organizations-structure/organization-role-mapping-dialog/organization-role-mapping-dialog.module.ts
+++ src/app/modules/organizations-structure/organization-role-mapping-dialog/organization-role-mapping-dialog.module.ts
@@ -7,10 +7,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatTreeModule } from '@angular/material/tree';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { HeaderModule } from '../../../layout/components/header/header.module';
 
 @NgModule({
     declarations: [OrganizationRoleMappingDialogComponent],
-    imports: [CommonModule, MatIconModule, MatProgressSpinnerModule, MatButtonModule, MatChipsModule, MatTreeModule, MatDividerModule],
+    imports: [CommonModule, MatIconModule, MatProgressSpinnerModule, MatButtonModule, MatChipsModule, MatTreeModule, MatDividerModule, MatTooltipModule, HeaderModule],
     entryComponents: [OrganizationRoleMappingDialogComponent],
 })
 export class OrganizationRoleMappingDialogModule {}
