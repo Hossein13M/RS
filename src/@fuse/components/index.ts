882731eb55955431086cd649c337b14779b2b072
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-11-01 11:05:54 +0330

    refactor(fuse): remove useless confirm dialog from fuse

--- src/@fuse/components/index.ts
+++ src/@fuse/components/index.ts
@@ -1,4 +1,3 @@
-export * from './confirm-dialog/confirm-dialog.module';
 export * from './countdown/countdown.module';
 export * from './demo/demo.module';
 export * from './highlight/highlight.module';
