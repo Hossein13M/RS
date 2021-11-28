71310de2a4d102234045aef5407f7372489f5b83
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-11-28 10:31:21 +0330

    fix(environment): remove useless env url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,4 @@ export const environment = {
     production: true,
     // serviceUrl: 'http://dev.management-api.iirms.ir'
     serviceUrl: 'http://172.22.255.239:3003',
-    // serviceUrl: 'http://172.21.253.98:3003'
 };
