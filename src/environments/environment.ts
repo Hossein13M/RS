50b746f4f3fb695a5bc2d367523c3455f13dfd88
Hossein Mousavi <dev.hosseinmousavi@gmail.com>
2021-12-13 12:42:49 +0330

    fix(environment): change environment IP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // serviceUrl: 'http://dev.management-api.iirms.ir'
-    serviceUrl: 'http://172.22.255.239:3003',
+    serviceUrl: 'http://dev.management-api.iirms.ir',
+    // serviceUrl: 'http://172.22.255.239:3003',
 };
